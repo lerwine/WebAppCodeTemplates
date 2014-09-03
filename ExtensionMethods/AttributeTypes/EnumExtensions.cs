3b776aa42e64572d6e19f35a77faaf4a5f857575
lenny@erwinefamily.net <lenny@erwinefamily.net>
2014-09-03 08:00:38 -0400

    Fixed bugs which prevented a successful build

--- ExtensionMethods/AttributeTypes/EnumExtensions.cs
+++ ExtensionMethods/AttributeTypes/EnumExtensions.cs
@@ -7,17 +7,6 @@ namespace Erwine.Leonard.T.ExtensionMethods.AttributeTypes
 {
     public static class EnumExtensions
     {
-        public static string GetEnumDescription<TEnum>(this TEnum value)
-            where TEnum : struct, IComparable, IFormattable, IConvertible
-        {
-            DescriptionAttribute da = value.GetEnumAttributes<TEnum, DescriptionAttribute>().FirstOrDefault(a => !String.IsNullOrWhiteSpace(a.Description));
-
-            if (da != null)
-                return da.Description;
-
-            return Enum.GetName(value.GetType(), value).Replace("_", " ");
-        }
-
         public static TAttribute[] GetEnumAttributes<TEnum, TAttribute>(this TEnum value)
             where TEnum : struct, IComparable, IFormattable, IConvertible
             where TAttribute : Attribute
---
+++ LoggingModule/ExtensionMethods/DescriptionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Erwine.Leonard.T.ExtensionMethods.AttributeTypes;
+
+namespace Erwine.Leonard.T.LoggingModule.ExtensionMethods
+{
+    public static class DescriptionExtensions
+    {
+        public static string GetEnumDescription<TEnum>(this TEnum value)
+            where TEnum : struct, IComparable, IFormattable, IConvertible
+        {
+            DescriptionAttribute da = value.GetEnumAttributes<TEnum, DescriptionAttribute>().FirstOrDefault(a => !String.IsNullOrWhiteSpace(a.Description));
+
+            if (da != null)
+                return da.Description;
+
+            return Enum.GetName(value.GetType(), value).Replace("_", " ");
+        }
+
+    }
+}
